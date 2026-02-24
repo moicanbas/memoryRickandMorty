9aab7d3e74335b1ec0ecad0ca12cbad1d34fc0c5
Moisés Cantillo <109709947+moicanbas@users.noreply.github.com>
2026-02-24 09:19:17 -0500

    Update Card.tsx
    cualquier cosa

--- src/views/home/components/Card.tsx
+++ src/views/home/components/Card.tsx
@@ -24,7 +24,9 @@ export default function Card({ image, flipped, disabled, onClick }: CardProps) {
           src={image}
           className="absolute inset-0 w-full h-full object-cover rounded-xl backface-hidden rotate-y-180"
         />
+        
       </div>
+
     </div>
   );
 }
